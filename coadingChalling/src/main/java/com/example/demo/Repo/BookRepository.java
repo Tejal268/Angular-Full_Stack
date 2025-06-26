a74dca4406357c1865cfaecddc7ee29a3d894e36
Tejal268 <88198504+Tejal268@users.noreply.github.com>
2025-06-26 16:34:53 +0530

    Delete coadingChalling directory

--- coadingChalling/src/main/java/com/example/demo/Repo/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Book;
-
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
